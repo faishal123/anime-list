216bfe355d830b3651dfd073cf842c1e9cdc7a0a
faishal123 <faisal@tantowi.com>
2022-05-08 01:15:24 +0700

    add web title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import client from "../src/graphql/client";
+import Head from "next/head";
 import { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>Animu</title>
+      </Head>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
   );
 }
 
